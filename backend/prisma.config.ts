f748c74ee582d53936ec58585212e8e4e19721cd
Amine <mohamedytreza465@gmail.com>
2026-07-13 11:56:54 +0100

    initialisation de l'ORM prisma

---
+++ backend/prisma.config.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { defineConfig } from "@prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrationsPath: "prisma/migrations",
+  datasource: {
+    url: process.env.DATABASE_URL || process.env["DATABASE_URL"],
+  },
+});
